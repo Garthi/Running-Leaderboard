7f18d3068d727251eee7850a908425d17912a3c4
Garthi <garth@new-crusader.de>
2015-08-11 23:38:54 +0200

    #3 add AbstractPagerAdapter and AbstractPagerPage classes

--- src/main/java/os/running/leaderboard/app/base/LeaderBoardPagerAdapter.java
+++ src/main/java/os/running/leaderboard/app/base/LeaderBoardPagerAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,40 +16,29 @@ import java.util.Map;
 /**
  * @author Martin "Garth" Zander <garth@new-crusader.de>
  */
-public class LeaderBoardPagerAdapter extends FragmentStatePagerAdapter
+public class LeaderBoardPagerAdapter extends AbstractPagerAdapter
 {
-    final int PAGE_COUNT = 4;
-    
-    private Integer tabTitles[] = new Integer[] {
-            R.string.leader_board_this_week,
-            R.string.leader_board_last_week,
-            R.string.leader_board_this_month,
-            R.string.leader_board_last_month
-    };
-    private Context context;
-    
-    private Map<Integer, LeaderBoardPage> fragment = new HashMap<Integer, LeaderBoardPage>();
-    
-    private Map<Integer, JSONObject> data = new HashMap<Integer, JSONObject>();
+    protected Map<Integer, JSONObject> data = new HashMap<Integer, JSONObject>();
 
     public LeaderBoardPagerAdapter(FragmentManager fm, Context context)
     {
-        super(fm);
-        this.context = context;
+        super(fm, context);
+
+        PAGE_COUNT = 4;
+        tabTitles = new Integer[] {
+                R.string.leader_board_this_week,
+                R.string.leader_board_last_week,
+                R.string.leader_board_this_month,
+                R.string.leader_board_last_month
+        };
         
         new createContent().execute();
     }
 
     @Override
-    public int getCount()
-    {
-        return PAGE_COUNT;
-    }
-
-    @Override
     public Fragment getItem(int position)
     {
-        LeaderBoardPage fragment = LeaderBoardPage.newInstance(position + 1);
+        LeaderBoardPage fragment = new LeaderBoardPage();
         
         this.fragment.put(position, fragment);
 
@@ -59,13 +47,6 @@ public class LeaderBoardPagerAdapter extends FragmentStatePagerAdapter
         }
         
         return fragment;
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position)
-    {
-        // Generate title based on item position
-        return context.getString(tabTitles[position]);
     }
 
     private void createContent(JSONObject result)
@@ -84,7 +65,7 @@ public class LeaderBoardPagerAdapter extends FragmentStatePagerAdapter
                 this.data.put(i, entry);
                 
                 if (this.fragment != null && this.fragment.size() >= i) {
-                    LeaderBoardPage fragment = this.fragment.get(i);
+                    LeaderBoardPage fragment = (LeaderBoardPage)this.fragment.get(i);
                     if (fragment != null) {
                         fragment.setContentData(entry);
                     }
